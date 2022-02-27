63096b164fa56ef6f2b4f05ef84f52eede392462
Subhanshu1 <49100597+Subhanshu1@users.noreply.github.com>
2022-02-27 23:25:02 +0530

    Add files via upload

--- database.js
+++ database.js
@@ -1,41 +1,42 @@
-const books =[
-    {
-        ISBN : "12345Book",
-        title : "Getting Started with MERN",
-        pubDate : "2021-11-25",
-        language : "en",
-        numPage : 250,
-        authors : [1,2],
-        publication :[1],
-        category : ["tech","programming","education"]
-    }
-]
+const books = [
+  {
+    ISBN: "12345Book",
+    title: "Getting started with MERN",
+    pubDate: "2021-11-25",
+    language: "en",
+    numPage: 250,
+    author: [1,2],
+    publications: [1],
+    category: ["tech", "programming", "education"]
+  }
+];
 
-const authors= [
-    {
-        id : 1,
-        name : "Subhanshu",
-        books: ["12345Book"]
-
-    },
-    {
-        id : 2,
-        name : "Aradhana",
-        books : ["12345Book"]
-    }
-]
+const author = [
+  {
+    id: 1,
+    name: "Aradhana",
+    books: ["12345Book", "MyBook"]
+  },
+  {
+    id: 2,
+    name: "Elon Musk",
+    books: ["12345Book"]
+  }
+];
 
 const publication = [
-    {
-        id : 1,
-        name : "Writex",
-        books : ["12345Book"]
-    },
-    {
-        id : 2,
-        name : "Writex",
-        books : [ ]
-    }
-]
+  {
+    id: 1,
+    name: "Writex",
+    books: ["12345Book"]
+  },
+  {
+    id: 2,
+    name: "Writex2",
+    books: []
+  }
+];
 
-module.exports = {books,authors,publication};
+module.exports = {books, author, publication};
+
+//mongoose -> Validation
